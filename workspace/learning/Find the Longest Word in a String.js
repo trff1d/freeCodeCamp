47b1fa1d39b32ac56726a8c5939d8d7792d34b68
trff1d <darren.ledger@tomtom.com>
2016-12-22 11:19:48 +0100

    Add files via upload

--- workspace/learning/Find the Longest Word in a String.js
+++ workspace/learning/Find the Longest Word in a String.js
@@ -1,5 +1,4 @@
 function findLongestWord(str) {
-	
 	var arrSentence = Array(str.split(" "));
 	var longestLengthSoFar = 0;
 
@@ -8,21 +7,15 @@ function findLongestWord(str) {
 	console.log("\n");
 	
 	for (var i = 0; i < arrSentence[0].length; i++) {
-		
-		console.log(arrSentence[0][i].length);
-		
+		console.log(arrSentence[0][i].length);		
 		if (arrSentence[0][i].length > longestLengthSoFar) {
 			longestLengthSoFar = arrSentence[0][i].length;
-			
 		}	
-		
 	}
-	
 	//return str.length;
 	console.log("\n");
 	return longestLengthSoFar;
 }
-
 console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
 console.log(findLongestWord("May the force be with you"));
 console.log(findLongestWord("Google do a barrel roll"));
